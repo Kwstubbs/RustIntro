4f683811d6fb7572de79d6fc71b607e030fae9a0
Kevin Stubbings <kwstubbings@protonmail.com>
2020-04-14 13:04:25 -0700

    discarded extraneous

--- webserver/target/debug/deps/webserver.d
+++
@@ -1,5 +0,0 @@
-C:\Users\K-stubbs\Documents\GitHub\RustIntro\webserver\target\debug\deps\webserver.exe: src\main.rs
-
-C:\Users\K-stubbs\Documents\GitHub\RustIntro\webserver\target\debug\deps\webserver.d: src\main.rs
-
-src\main.rs:
--- webserver/target/debug/webserver.d
+++
@@ -1,1 +0,0 @@
-C:\Users\K-stubbs\Documents\GitHub\RustIntro\webserver\target\debug\webserver.exe: C:\Users\K-stubbs\Documents\GitHub\RustIntro\webserver\src\main.rs
